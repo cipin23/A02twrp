8b9c536381a63982624007eeacd2c173c4a1cf4f
cipin23 <149391390+cipin23@users.noreply.github.com>
2025-12-31 23:48:17 +0700

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,14 @@
 #
 # Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 SebaUbuntu's omni device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_a02.mk
+    $(LOCAL_DIR)/omni_a02.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_a02-user \
-    twrp_a02-userdebug \
-    twrp_a02-eng
+    omni_a02-user \
+    omni_a02-userdebug \
+    omni_a02-eng
