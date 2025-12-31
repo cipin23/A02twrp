f61baf08aedcda459a2eb5d8398672e6358b6f85
cipin23 <149391390+cipin23@users.noreply.github.com>
2025-12-31 23:47:46 +0700

    Update and rename twrp_a02.mk to omni_a02.mk

--- omni_a02.mk
+++ omni_a02.mk
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2023 The Android Open Source Project
-# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2023 SebaUbuntu's omni device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, device/samsung/a02/device.mk)
 
 PRODUCT_DEVICE := a02
-PRODUCT_NAME := twrp_a02
+PRODUCT_NAME := omni_a02
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A022F
 PRODUCT_MANUFACTURER := samsung
